ca619ed58030ede0a20db6a5479bb4c48a0208b6
smart-sys-dev <novble2@gmail.com>
2021-11-27 14:20:32 +0700

    +

---
+++ main.cpp
@@ -0,0 +1,14 @@
+
+
+#include <sstream>
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stdexcept>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+	return 0;
+}
